340d0961841008de96d1e494a64aa2a3fbfba748
HichamMad <hicham.elmadani.ing@gmail.com>
2023-10-05 19:01:30 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - this is the main put function
  * Return: 0
@@ -7,4 +9,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
+
 
