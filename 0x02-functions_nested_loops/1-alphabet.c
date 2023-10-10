944734444b28fa2df3375cba04337306892e9817
HichamMad <hicham.elmadani.ing@gmail.com>
2023-10-10 16:04:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Print the alphabets in lowercase followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z' ; alph++)
+		_putchar(alph);
+	_putchar('\n');
+}
