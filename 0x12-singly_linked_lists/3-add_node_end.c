e1583ea085344b6a706bee43ed4fdfef1168e9a5
HichamMad <hicham.elmadani.ing@gmail.com>
2023-11-16 17:45:51 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,13 +11,12 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *temp = *head;
 	list_t *new;
-	list_t *temp;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
-	temp = *head;
 	new->str = strdup(str);
 	new->len = _strlen(str);
 	new->next = NULL;
@@ -33,7 +32,7 @@ list_t *add_node_end(list_t **head, const char *str)
 }
 
 /**
- * len - Calculates the length of a string.
+ * _strlen - Calculates the length of a string.
  * @str: Constant string
  * Return: The length of the string
  */
